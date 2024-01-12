88aad64ee3e42e80256f1c460fefb26030229cdb
David Vinson <davidvinson018@gmail.com>
2024-01-12 16:53:48 -0600

    get data to client via axios get

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,35 @@
-console.log('client.js is sourced!');
+function onReady() {
+  console.log("client.js is sourced!");
+  // get DOM element where calc history will go
+  let resultHistory = document.getElementById("resultHistory");
+  let recentResult = document.getElementById("recentResult");
+
+  const resultData = getCalcData();
+  if (resultData) {
+    console.log("Put this on the DOM", resultData);
+    const mostRecent = getRecentResult(resultData);
+    recentResult.append(mostRecent);
+    resultHistory.append(resultData);
+  } else {
+    console.log("Nothing to see here..");
+    resultHistory.innerHTML = "Nothing to see here..";
+  }
+}
+
+function getCalcData() {
+  console.log("go get calc history from server...");
+  axios
+    .get("/calculations")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw new Error(`Server Error: ${error}`);
+    });
+}
+
+function getRecentResult(calcHistory) {
+  return calcHistory.pop();
+}
+
+onReady();
