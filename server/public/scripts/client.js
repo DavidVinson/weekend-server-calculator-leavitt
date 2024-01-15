b0b0f80c3a2869a3e5ec97722e298055989949d3
David Vinson <davidvinson018@gmail.com>
2024-01-15 14:03:31 -0600

    frontend work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,34 +2,34 @@ function onReady() {
   console.log("client.js is sourced!");
   // get DOM element where calc history will go
   let resultHistory = document.getElementById("resultHistory");
-  let recentResult = document.getElementById("recentResult");
+  // let recentResult = document.getElementById("recentResult");
 
-  const resultData = getCalcData();
-  if (resultData) {
-    console.log("Put this on the DOM", resultData);
-    const mostRecent = getRecentResult(resultData);
-    recentResult.append(mostRecent);
-    resultHistory.append(resultData);
-  } else {
-    console.log("Nothing to see here..");
-    resultHistory.innerHTML = "Nothing to see here..";
-  }
-}
-
-function getCalcData() {
-  console.log("go get calc history from server...");
   axios
     .get("/calculations")
     .then((response) => {
-      return response.data;
+      // console.log("calc data response", response.data);
+      const resultData = response.data;
+      if (resultData) {
+        console.log("Put this on the DOM", resultData);
+        // const mostRecent = getRecentResult(resultData);
+        // recentResult.append(mostRecent);
+        // resultHistory.append(resultData);
+      } else {
+        console.log("Nothing to see here..");
+        resultHistory.innerHTML = "Nothing to see here..";
+      }
     })
     .catch((error) => {
       throw new Error(`Server Error: ${error}`);
     });
 }
 
-function getRecentResult(calcHistory) {
-  return calcHistory.pop();
-}
+// function getCalcData() {
+//   console.log("go get calc history from server...");
+// }
+
+// function getRecentResult(calcHistory) {
+//   return calcHistory.pop();
+// }
 
 onReady();
